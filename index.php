7fa946b54dd04b10980b7105e6e36285aceb7552
paulwenny <pollywenny@gmail.com>
2023-12-19 03:00:35 +0800

    Menambahkan file index.html dan invois.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "db_penjualan");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Table Transaksi</h1>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>No Transaksi</th>
+                <th>Nama Konsumen</th>
+                <th>Tanggal</th>
+                <th>Graund Total</th>
+                <th>Nama Kasir</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                $tampildata = mysqli_query($conn, "SELECT * FROM tb_penjualan");
+                while($data = mysqli_fetch_array($tampildata)){
+            ?>
+                    
+                    <tr>
+                        <td><?= $data['nomor_transaksi'] ?></td>
+                        <td><?= $data['nama_konsumen'] ?></td>
+                        <td><?= $data['tanggal_transaksi'] ?></td>
+                        <td><?= $data['grend_total'] ?></td>
+                        <td><?= $data['nama_kasir'] ?></td>
+                        <td><a href="invois.php">Detail</a></td>
+                    </tr>
+            <?php
+                }
+            ?>
+
+        </tbody>
+    </table>
+</body>
+</html>
---
+++ invois.php
@@ -0,0 +1,60 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "db_penjualan");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Invois</h1>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>No Transaksi</th>
+                <th>Nama Konsumen</th>
+                <th>Tanggal</th>
+                <th>Graund Total</th>
+                <th>Nama Kasir</th>
+                
+                <th>Produk</th>
+                <th>Harga</th>
+                <th>Jumlah Beli</th>
+                <th>Merek</th>
+
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                $tampildata = mysqli_query($conn, "SELECT * FROM tb_penjualan
+                    INNER JOIN tb_detail_transaksi ON tb_detail_transaksi.id = tb_penjualan.id
+                ");
+                while($data = mysqli_fetch_array($tampildata)){
+            ?>
+                    
+                    <tr>
+                        <td><?= $data['nomor_transaksi'] ?></td>
+                        <td><?= $data['nama_konsumen'] ?></td>
+                        <td><?= $data['tanggal_transaksi'] ?></td>
+                        <td><?= $data['grend_total'] ?></td>
+                        <td><?= $data['nama_kasir'] ?></td>
+
+                        <td><?= $data['produk'] ?></td>
+                        <td><?= $data['harga'] ?></td>
+                        <td><?= $data['jumlah_beli'] ?></td>
+                        <td><?= $data['merek'] ?></td>
+
+                        <td><a href="index.php">Back</a></td>
+                    </tr>
+            <?php
+                }
+            ?>
+
+        </tbody>
+    </table>
+</body>
+</html>
